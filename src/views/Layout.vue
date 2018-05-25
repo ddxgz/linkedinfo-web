9fd74eafa0bb7594bae757473a672a71db3f0f21
ddxgz <ddxgz19880813@gmail.com>
2018-05-25 21:39:24 +0200

    add cookie alert

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -2,6 +2,18 @@
   <div>
     <!-- <b-container> -->
     <navbar></navbar>
+    <!-- <b-container> -->
+      <!-- <div v-if="notCookieConsent"> -->
+    <!-- <b-alert show dismissible fade> -->
+    <b-alert :show="showAlert" fade>
+       <!-- <h4 class="alert-heading">We use cookie!</h4> -->
+        LinkedInfo.co uses cookie only for the language preference. We do not 
+        collect or store any user data on our server.
+          <b-button class="mx-3 my-0"
+            @click="agreeCookie">Agree</b-button>
+    </b-alert>
+      <!-- </div> -->
+    <!-- </b-container> -->
     <app-main></app-main>
     <vm-back-top :duration="300"></vm-back-top>
     <footer-block></footer-block>
@@ -12,6 +24,7 @@
 <script>
 import VmBackTop from 'vue-multiple-back-top';
 import { Navbar, AppMain, FooterBlock } from './components';
+import {getCookie, setCookie} from '@/utils/cookie'
 
 export default {
   name: 'layout',
@@ -20,6 +33,29 @@ export default {
     AppMain,
     VmBackTop,
     FooterBlock
+  },
+  data() {
+    return {
+      showAlert: true
+    }
+  },
+  created() {
+    this.notCookieConsent();
+  },
+  methods: {
+    notCookieConsent: function() {
+    // showAlert: function() {
+      var consent = getCookie('cookieConsent')
+      if (consent === '1') {
+        // return false
+        this.showAlert = false
+      }
+      // return true;
+    },
+    agreeCookie: function() {
+      setCookie('cookieConsent', '1', 7);
+      this.showAlert = false;
+    }
   }
 }
 </script>
